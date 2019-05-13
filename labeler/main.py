0bf32380327cff24a289f518778d6db29bf459c6
alexfaus08 <alexfaus08@gmail.com>
2019-05-13 14:10:52 -0400

    comments

--- labeler/main.py
+++ labeler/main.py
@@ -5,13 +5,16 @@ import pdb
 ''' Double click to place a point. Place four points one at each corner of the Scrabble board. The order of the points 
 placed matters. The order is as follows: top left, top right, bottom left, bottom right.'''
 
+# this class is used to store four points that make up the corners of a Scrabble board
 class FourPoints:
     def __init__(self):
         self.points = []
 
+    # add a circle to the image based on where the mouse double clicks
     def add_point(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDBLCLK:
-            cv2.circle(img, (x, y), 5, (255, 0, 0), -1)
+            # x, y pos of mouse, 2 is the radius of the circle, the rest of the parameters are color
+            cv2.circle(img, (x, y), 2, (255, 0, 0), -1)
             self.points.append([x,y])
 
     def get_points(self):
@@ -23,25 +26,34 @@ fourpoints = FourPoints()
 # show the image
 img = cv2.imread("/Users/Alex/Desktop/Summer 2019/scrabble/data/Photo_2006-03-10_002.jpg", 0)
 wd, ht = img.shape
+# resize the image to fit for the window
+img = cv2.resize(img, (640, 480))
 cv2.namedWindow('image')
+# this tracks the mouse position
 cv2.setMouseCallback('image', fourpoints.add_point)
 
 while(1):
-    # hit escape to exit
     cv2.imshow('image',img)
+    # if esc is hit, exit
     if cv2.waitKey(20) & 0xFF == 27:
         break
+    # if four points are placed stop waiting for points
     if len(fourpoints.get_points()) == 4:
         break
 
 print(fourpoints.get_points())
 
-i = 1000
+# i is the width and the height of the resulting image
+# since scrabble is 15 by 15 i should be divisible by 15
+i = 825
 pts1 = np.float32(fourpoints.get_points())
 pts2 = np.float32([[0,0],[i,0],[0,i],[i,i]])
-M = cv2.getPerspectiveTransform(pts1,pts2)
 
+# M is the perspective matrix
+M = cv2.getPerspectiveTransform(pts1,pts2)
+# dst is the resulting flat image
 dst = cv2.warpPerspective(img, M, (i, i))
+
 cv2.destroyAllWindows()
 cv2.imshow('output', dst)
 cv2.waitKey(0)
