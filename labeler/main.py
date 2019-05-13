bfcd1ac009c798aabc359c09a65b0c194da9eb29
alexfaus08 <alexfaus08@gmail.com>
2019-05-13 12:40:15 -0400

    save 4 points

--- labeler/main.py
+++ labeler/main.py
@@ -1,22 +1,31 @@
 import cv2
 import numpy as np
-from pyimagesearch.transform import four_point_transform
 
 
+class FourPoints:
+    def __init__(self):
+        self.points = []
 
-def add_point(event, x, y, flags, param):
-    global ix, iy
-    if event == cv2.EVENT_LBUTTONDBLCLK:
-        cv2.circle(img, (x,y), 5, (255,0,0), -1)
-        ix, iy = x,y
+    def add_point(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDBLCLK:
+            cv2.circle(img, (x, y), 5, (255, 0, 0), -1)
+            self.points.append((x,y))
 
+    def get_points(self):
+        return self.points
+
+
+fourpoints = FourPoints()
 
 # show the image
 img = cv2.imread("/Users/Alex/Desktop/Summer 2019/scrabble/data/Photo_2006-03-10_002.jpg", 0)
 cv2.namedWindow('image')
-cv2.setMouseCallback('image', add_point)
+cv2.setMouseCallback('image', fourpoints.add_point)
 
 while(1):
+    # hit escape to exit
     cv2.imshow('image',img)
     if cv2.waitKey(20) & 0xFF == 27:
         break
+
+print(fourpoints.get_points())
