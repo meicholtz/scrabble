343da85fb6e7cfe34074e35b86e62c15146055ea
alexfaus08 <alexfaus08@gmail.com>
2019-05-14 12:05:48 -0400

    labeller done

--- labeler/main.py
+++ labeler/main.py
@@ -1,30 +1,13 @@
 import cv2
 import numpy as np
 import pdb
-import glob
 import argparse
+import glob
 import os
+import atexit
 
 ''' Double click to place a point. Place four points one at each corner of the Scrabble board. The order of the points 
 placed matters. The order is as follows: top left, top right, bottom left, bottom right.'''
-
-# this class is used to store four points that make up the corners of a Scrabble board
-class FourPoints:
-    def __init__(self):
-        self.points = []
-
-    # add a circle to the image based on where the mouse double clicks
-    def add_point(self, event, x, y, flags, param):
-        if event == cv2.EVENT_LBUTTONDBLCLK:
-            # x, y pos of mouse, 2 is the radius of the circle, the rest of the parameters are color
-            cv2.circle(img, (x, y), 2, (255, 0, 0), -1)
-            self.points.append([x,y])
-
-    def get_points(self):
-        return self.points
-
-    def new_points(self):
-        self.points = []
 
 
 # set up argparse
@@ -34,19 +17,82 @@ parser = argparse.ArgumentParser(description='Double click to place a point. Pla
                                              'bottom left, bottom right.')
 
 parser.add_argument('-d', '--directory', type=str, help='The directory containing images you want to label.',
-                    default=os.path.join(os.getcwd(), '/data'))
+                    default=os.path.dirname(os.getcwd()) + '/data')
 parser.add_argument('-o', '--order', type=str, help='The order in which you want to label images.',
                     choices=['top', 'bottom'], default='top')
-parser.add_argument('-f', '--file', help='The output file to write labels.', type=str, default='labels.txt')
+parser.add_argument('-f', '--file', help='The output file to write labels.', type=str,
+                    default=os.path.join(os.getcwd(), 'labels.txt'))
 parser.print_help()
 args = parser.parse_args()
 
+# this class is used to store four points that make up the corners of a Scrabble board
+class FourPoints:
+    def __init__(self):
+        self.points = []
+        self.img = ''
+
+    # add a circle to the image based on where the mouse double clicks
+    def add_point(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDBLCLK:
+            # x, y pos of mouse, 2 is the radius of the circle, the rest of the parameters are color
+            cv2.circle(self.img, (x, y), 2, (255, 0, 0), -1)
+            self.points.append([x,y])
+
+    def get_points(self):
+        return self.points
+
+    def new_points(self):
+        self.points = []
+
+    def set_img(self, i):
+        self.img = i
+
+
 def main(args):
+    # from argparser
     dir = args.directory
     order = args.order
     file = args.file
 
+
+    # check to see if directory exists
+    if(not os.path.isdir(dir)):
+        raise Exception('Directory {} does not exist'.format(dir))
+
+    # the w+ indicates that if the file does not exist, create it and also this file will be written to
+    f = open(file, "a+")
+    # all of the names of images
+    f.seek(0)
+    labelled = [x.split(' ')[0] for x in f.readlines()]
+    pdb.set_trace()
+    images = glob.glob(dir + "/*.jpg")
+    # instantiate the four points class
     fourpoints = FourPoints()
+
+    # for each image in the directory:
+    for i in images:
+        # check to see if image has been labelled
+        if not os.path.basename(i) in labelled:
+            img = cv2.imread(i, 0)
+            # resize the image to fit for the window
+            img = cv2.resize(img, (640, 480))
+            cv2.namedWindow('image')
+            fourpoints.set_img(img)
+            # this tracks the mouse position
+            cv2.setMouseCallback('image', fourpoints.add_point)
+            cont = True
+            while (cont):
+                cv2.imshow('image', img)
+                # if esc is hit, exit
+                if cv2.waitKey(20) & 0xFF == 27:
+                    cont = False
+                # if four points are placed stop waiting for points
+                if len(fourpoints.get_points()) == 4:
+                    # add the four points and file name to text file
+                    f.write(os.path.basename(i) + " " + str(fourpoints.get_points()) + '\n')
+                    f.flush()
+                    fourpoints.new_points()
+                    cont = False
 
 
     # show the image
@@ -68,6 +114,8 @@ def main(args):
             break
 
     print(fourpoints.get_points())
+    strr = str(fourpoints.get_points())
+    f.write()
 
     # i is the width and the height of the resulting image
     # since scrabble is 15 by 15 i should be divisible by 15
