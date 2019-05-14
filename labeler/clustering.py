29b581189063ab8895a35e8df7967a5096fcdc3d
alexfaus08 <alexfaus08@gmail.com>
2019-05-14 15:51:17 -0400

    points extracted from text file

--- labeler/clustering.py
+++ labeler/clustering.py
@@ -3,4 +3,15 @@ import cv2
 from sklearn.decomposition import IncrementalPCA
 from sklearn.cluster import KMeans
 import glob
+import pdb
+import re
 
+# gather images that have been labelled
+f = open('labels.txt')
+
+for line in f.readlines():
+    strr = ''
+    x = line.split()
+    x = x[1:]
+    pts = strr.join(x)
+    pts = eval(pts)
