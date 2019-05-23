459ddc6fbcbfa129824459279197e059db71038d
Matthew Eicholtz <meicholtz@flsouthern.edu>
2019-05-23 00:07:59 -0400

    added comments and spacing

--- test/montage.py
+++ test/montage.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python
+
+'''Show montage of 'images'.'''
+
 from skimage.util import montage
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(6*3*3).reshape((6,3,3))
+
+x = np.arange(6 * 3 * 3).reshape((6, 3, 3))
 print(x)
 
 y = montage(x, grid_shape=(2, 3))
