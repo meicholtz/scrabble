fe9491b8ae001d7da9eb9cbfa077f0e2f8286fff
alexfaus08 <alexfaus08@gmail.com>
2019-05-23 23:52:44 -0400

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -23,27 +23,18 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 cv2.imshow("Image", gray)
 
-# check to see if we should apply thresholding to preprocess the
-# image
-if args["preprocess"] == "thresh":
-	gray = cv2.threshold(gray, 0, 255,
+# apply thresholding
+gray = cv2.threshold(gray, 0, 255,
 		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
-# make a check to see if median blurring should be done to remove
-# noise
-elif args["preprocess"] == "blur":
-	gray = cv2.medianBlur(gray, 3)
+# blurring should be done to remove noise
+gray = cv2.medianBlur(gray, 3)
+img = []
 
-# write the grayscale image to disk as a temporary file so we can
-# apply OCR to it
-filename = "{}.png".format(os.getpid())
-cv2.imwrite(filename, gray)
-
-# load the image as a PIL/Pillow image, apply OCR, and then delete
-# the temporary file
-text = pytesseract.image_to_string(Image.open(filename))
-os.remove(filename)
+# load numpy images to be OCR'd
+text = pytesseract.image_to_string(Image.fromarray(img))
 print(text)
+
 
 # show the output images
 # cv2.imshow("Image", image)
