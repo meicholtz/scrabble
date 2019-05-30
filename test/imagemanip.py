c0c1f256dca276b89d67d8b5fd39166bde3b8a96
alexfaus08 <alexfaus08@gmail.com>
2019-05-30 13:48:48 -0400

    Update test/imagemanip.py

--- test/imagemanip.py
+++ test/imagemanip.py
@@ -2,23 +2,25 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import cv2
 import utils
+import numpy as np
+import pdb
 
 def change_pic():
-    vlabel.configure(image=root.photo1)
+    global img
+    grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    temp = Image.fromarray(grayImage)
+    vlabel.configure(image=ImageTk.PhotoImage(temp))
     print("updated")
 
 root = tk.Tk()
-
 img = utils.get_board('/Users/Alex/Desktop/Summer-2019/scrabble/labels.txt', 0)
 photo = Image.fromarray(img)
-grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-root.photo = ImageTk.PhotoImage(photo)
-root.photo1 = ImageTk.PhotoImage(Image.fromarray(grayImage))
 
+root.photo = ImageTk.PhotoImage(photo)
 vlabel=tk.Label(root,image=root.photo)
 vlabel.pack()
 
-b2=tk.Button(root,text="Threshold",command=change_pic)
+b2=tk.Button(root,text="BW",command=change_pic)
 b2.pack()
 
 root.mainloop()
