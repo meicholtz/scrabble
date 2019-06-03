87b69d9cfe2021a8753fa0dac932cdf35ee9abe2
Matthew Eicholtz <meicholtz@flsouthern.edu>
2019-06-03 18:40:12 -0400

    used imshow from utils

--- test/thresh.py
+++ test/thresh.py
@@ -7,12 +7,11 @@ import PIL
 import pytesseract
 
 # this is for sliders
+
+
 def nothing(x):
     pass
 
-def show(img, title="show"):
-    cv2.imshow(title, img)
-    cv2.waitKey(0)
 
 def preprocess(image):
     # get the width and height
@@ -20,7 +19,7 @@ def preprocess(image):
     # make image BW
     img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Resize the img
-    img = cv2.resize(img, (w -150, h -150))
+    img = cv2.resize(img, (w - 150, h - 150))
     cv2.namedWindow('image')
     # create trackbar for threshold and thinning parameters
     cv2.createTrackbar('Threshold', 'image', 60, 255, nothing)
@@ -29,16 +28,16 @@ def preprocess(image):
     cv2.createTrackbar('invert', 'image', 0, 1, nothing)
     flag = True
     kernel = np.ones((2, 2), np.uint8)
-    while(1):
+    while True:
         # check the positions of the trackbars and store them
         x = cv2.getTrackbarPos('Threshold', 'image')
         y = cv2.getTrackbarPos('Thinning', 'image')
         invert = cv2.getTrackbarPos('invert', 'image')
         # apply filtering with trackbar parameters
-        if(invert == 1 and flag):
+        if invert == 1 and flag:
             img = 255 - img
             flag = False
-        if(invert == 0):
+        if invert == 0:
             flag = True
         ret, temp = cv2.threshold(img, x, 255, cv2.THRESH_BINARY)
         temp2 = cv2.erode(temp, kernel, iterations=y)
@@ -51,37 +50,33 @@ def preprocess(image):
         if k == ord('d'):
             return temp2
 
+
 def ocr(square):
     # convert square to be ocr'd
     img = PIL.Image.fromarray(square)
     # get ocr label
     label = pytesseract.image_to_string(img, config='--psm 10')
     print(label)
-    show(square)
-
+    utils.imshow(square)
 
 
 path = os.path.join(os.path.dirname(os.getcwd()), 'labels.txt')
 test2 = utils.get_board(path, 2)
 img = preprocess(test2)
-show(img, title=str(2))
+utils.imshow(img, title=str(2))
 cv2.destroyAllWindows()
 sqs = utils.squares_from_img(img)
 for s in sqs:
-    w,h = s.shape[0], s.shape[1]
-    s = cv2.resize(s, (w*10, h*10))
+    w, h = s.shape[0], s.shape[1]
+    s = cv2.resize(s, (w * 10, h * 10))
     ocr(s)
 
 for x in range(0, 100):
     test2 = utils.get_board(path, x)
     img = preprocess(test2)
-    show(img, title=str(x))
+    utils.imshow(img, title=str(x))
     cv2.destroyAllWindows()
     if(x == 2):
         sqs = utils.squares_from_img(img)
         for s in sqs:
             ocr(s)
-
-
-
-
