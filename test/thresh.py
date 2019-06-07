82dcd8dc5af97d52bc1ad846c62430f4cac41a90
alexfaus08 <alexfaus08@gmail.com>
2019-06-07 13:32:28 -0400

    Update test/thresh.py

--- test/thresh.py
+++ test/thresh.py
@@ -13,6 +13,8 @@ def nothing(x):
 # callback function for Invert slider
 def invert_img(x):
     preprocess.img = 255 - preprocess.img
+    preprocess.invert = not preprocess.invert
+    print(preprocess.invert)
 
 # callback function for Adaptive Threshold slider
 # x is the value of the slider
@@ -38,6 +40,7 @@ def preprocess(image):
     # Resize the img
     preprocess.img = cv2.resize(preprocess.img, (w - 250, h - 250))
     preprocess.org = preprocess.img
+    preprocess.img = cv2.equalizeHist(preprocess.img)
     cv2.namedWindow('image')
     # create trackbar for threshold and thinning parameters
     cv2.createTrackbar('Adaptive Threshold', 'image', 0, 2, adaptive_threshold)
@@ -45,7 +48,8 @@ def preprocess(image):
     cv2.createTrackbar('Thinning', 'image', 0, 5, nothing)
     # create a slider that will invert the colors of the image
     cv2.createTrackbar('Invert', 'image', 0, 1, invert_img)
-    kernel = np.ones((2, 2), np.uint8)
+    kernel = np.ones((3, 3), np.uint8)
+    preprocess.invert = False
     while True:
         # check the positions of the trackbars and store them
         x = cv2.getTrackbarPos('Threshold', 'image')
@@ -60,14 +64,22 @@ def preprocess(image):
             break
         # if the user presses D stop the preprocessing and return the processed image
         if k == ord('d'):
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            if (preprocess.invert):
+                image = 255 - image
+            ret, temp = cv2.threshold(image, x, 255, cv2.THRESH_BINARY)
+            temp2 = cv2.dilate(temp, kernel, iterations=y)
             return temp2
+
 
 
 def ocr(square):
     # convert square to be ocr'd
     img = PIL.Image.fromarray(square)
+    # config string for tesseract
+    tessdata_dir_config = '--tessdata-dir "/usr/local/Cellar/tesseract/4.0.0_1/share/tessdata" --psm 10  --oem 2 '
     # get ocr label
-    label = pytesseract.image_to_string(img, config='--psm 10')
+    label = pytesseract.image_to_string(img, config=tessdata_dir_config)
     return label
 
 
@@ -75,20 +87,23 @@ def ocr(square):
 
 # get the path of the labels text file
 path = os.path.join(os.path.dirname(os.getcwd()), 'labels.txt')
-for x in range(0, 100):
+for ind in range(0, 100):
     # get the image of the board
-    test2 = utils.get_board(path, x)
+    test2 = utils.get_board(path, ind)
+    w, h = test2.shape[0], test2.shape[1]
     # preprocess the image
     img = preprocess(test2)
     cv2.destroyAllWindows()
     # get the individual tiles from the image
     sqs = utils.squares_from_img(img)
     # for each tile:
+    sq = 1
     for s in sqs:
         # get width and height
-        w,h = s.shape[0], s.shape[1]
-        # enlarge the tile by a factor of 10
-        s = cv2.resize(s, (w*10, h*10))
+        s_w,s_h = s.shape[0], s.shape[1]
+        # get the float values for width and height
+        x, y = float(s_w / w), float(s_h / h)
+        
         # get the ocr of the tile
         print(ocr(s))
         cv2.imshow("square", s)
