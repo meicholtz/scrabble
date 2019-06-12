cc4ec507ca5c370e36889efb0b8205cbdbfabdcf
alexfaus08 <alexfaus08@gmail.com>
2019-06-12 13:28:13 -0400

    labeler

--- test/label_manually.py
+++ test/label_manually.py
@@ -2,6 +2,7 @@ import cv2
 import utils
 import os
 import numpy as np
+import ipdb
 import re
 
 
--- test/ocr_testing.py
+++ test/ocr_testing.py
@@ -1,12 +1,43 @@
-import tesseract
+import pytesseract
+import PIL
+import cv2
+import numpy as np
+import io
+import ipdb
+import re
+
+BLANK_LABEL = 'NONE'
 
 
+def filter_ocr(text):
+    if(text == '' or text == ' '):
+        return BLANK_LABEL
+    # grab the first character
+    t = text[0]
+    # remove non letter text
+    regex = re.compile('[^a-zA-Z]')
+    t = regex.sub('', t)
+    if (t == ''):
+        return BLANK_LABEL
+    # capitalize letter
+    t = t.capitalize()
+    return t
 
-api = tesseract.TessBaseAPI()
-api.Init(".","eng",tesseract.OEM_DEFAULT)
-api.SetVariable("tessedit_char_whitelist", "0123456789abcdefghijklmnopqrstuvwxyz")
-api.SetPageSegMode(tesseract.PSM_AUTO)
 
-mImgFile = "test.jpg"
-mBuffer=open(mImgFile,"rb").read()
-result = tesseract.ProcessPagesBuffer(mBuffer,len(mBuffer),api)
+file_path = "/Users/Alex/Downloads/yes.jpg"
+img = cv2.imread(file_path)
+w, h = img.shape[0], img.shape[1]
+# convert img to be ocr'd
+img = PIL.Image.fromarray(img)
+# config string for tesseract
+tessdata_dir_config = '--psm 11 -l eng'
+# get ocr label
+label = pytesseract.image_to_data(img, config=tessdata_dir_config)
+data = np.genfromtxt(io.BytesIO(label.encode()), delimiter="\t", skip_header=1, filling_values=1,
+                     usecols=(6, 7, 8, 9, 10, 11), dtype=np.str)
+# delete all the rows where tesseract did not find a letter
+data = data[np.where(data[:, 4] != '-1')]
+# for each row, filter the label that tesseract returned
+for i in range(0, len(data)):
+    data[i][5] = filter_ocr(data[i][5])
+data = data[np.where(data[:, 5] != BLANK_LABEL)]
