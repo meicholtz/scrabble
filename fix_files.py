f51f512a3fe24f94d739d005cc60b2e49507ff47
alexfaus08 <alexfaus08@gmail.com>
2019-06-26 11:38:55 -0400

    fixed all the files

--- fix_files.py
+++ fix_files.py
@@ -11,10 +11,16 @@ from utils import *
 directory = os.path.join(home(), 'labels')
 for file in os.listdir(directory):
     if(file != 'labels.txt' and file != 'labels1.txt' and file.endswith('.txt')):
-        f = open(file)
+        f = open(os.path.join(directory, file), "r")
         lines = f.readlines()
         for i in range(0, len(lines)):
             a = lines[i].split(' ')
             if(a[0] == 'NONE'):
                 a[0] = '~'
             x, y = float(a[1]), float(a[2])
+            swh = float(a[3])
+            x_max, y_max = x + swh, y + swh
+            lines[i] = '{0} {1:.4f} {2:.4f} {3:.4f} {4:.4f}\n'.format(a[0], x, y, x_max, y_max)
+        with open(os.path.join(directory, file), 'w') as f:
+                f.writelines(lines)
+        f.close()
