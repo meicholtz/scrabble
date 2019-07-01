a798a9df50b8409863505ff932b237fa80407199
Matthew Eicholtz <meicholtz@flsouthern.edu>
2019-07-01 12:20:02 -0400

    edited output to show letters

--- countletters.py
+++ countletters.py
@@ -1,19 +1,33 @@
+#!/usr/bin/env python
+
+'''Get basic statistics about labeled training data.'''
+
 from utils import *
 import os
 import numpy as np
 
+BLANK_LABEL = '~'  # string for tiles that do not contain a letter
 
-letters = np.zeros(26)
-label_files = ['labels.txt', 'labels1.txt']
-directory = os.path.join(home(), 'labels')
-for filename in os.listdir(directory):
-    if filename.endswith(".txt") and filename not in label_files:
-        f = open(os.path.join(directory, filename))
-        for line in f.readlines():
-            letter = line.split(' ')[0]
-            if(letter == 'NONE' or ord(letter) < 65):
-                continue
-            letters[ord(letter) - 65] += 1
-    else:
-        continue
-print(letters)
+
+def main():
+    root = os.path.join(home(), 'labels')  # directory containing labels
+    labelfiles = ['labels.txt', 'labels1.txt']  # do not include in statistics
+
+    # Compute count of each letter
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    cnt = {key: 0 for key in letters}
+    for file in os.listdir(root):
+        if file.endswith(".txt") and file not in labelfiles:
+            f = open(os.path.join(root, file))
+            for line in f.readlines():
+                letter = line.split(' ')[0]
+                if letter != BLANK_LABEL and letter in letters:
+                    cnt[letter] += 1
+
+    # Print output
+    for key in cnt:
+        print("{} {}".format(key, cnt[key]))
+
+
+if __name__ == '__main__':
+    main()
