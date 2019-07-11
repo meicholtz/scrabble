8a6fabf314b5ecf9a24e06c25b036a3730d10906
alexfaus08 <alexfaus08@gmail.com>
2019-07-11 13:19:55 -0400

    Update test/compare_data.py

--- test/compare_data.py
+++ test/compare_data.py
@@ -4,8 +4,10 @@ import ipdb
 
 my_data = np.load('/Users/Alex/Desktop/Summer-2019/scrabble/scrabble_dataset.npz', allow_pickle=True)
 data = np.load('/Users/Alex/Desktop/Summer-2019/scrabble/yad2k-em3d/data.npz', allow_pickle=True)
+
 print('My image shape: {} \nORNL image Shape: {}\n\n'.format(my_data['images'].shape, data['images'].shape))
 print('My shape of one image: {} \nORNL shape of one image: {}\n\n'.format(my_data['images'][0].shape, data['images'][0].shape))
+
 print('My boxes shape: {} \nORNL boxes Shape: {}\n\n'.format(my_data['boxes'].shape, data['boxes'].shape))
 print('My shape of one box: {} \nORNL shape of one box: {}\n\n'.format(my_data['boxes'][0].shape, data['boxes'][0].shape))
 ipdb.set_trace()
