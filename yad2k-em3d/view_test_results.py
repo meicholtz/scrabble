309c6b74ea763087b84a27455df351966d072d1d
alexfaus08 <alexfaus08@gmail.com>
2019-09-30 15:37:05 -0400

    Update yad2k-em3d/view_test_results.py

--- yad2k-em3d/view_test_results.py
+++ yad2k-em3d/view_test_results.py
@@ -40,6 +40,8 @@ def _main(args):
     images, boxes = utils.get_data(data_path)
     test_results = scipy.io.loadmat(test_results)
     t = test_results['output']
+    cv2.imshow("TESTING", images[20])
+    cv2.waitKey(0)
     ipdb.set_trace()
     classes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
     class_names = ['A','B','C','D','E','F','G','H','I','J','K','L','M',
